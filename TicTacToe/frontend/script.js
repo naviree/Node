98336567d767f2b64b5d2f0abb69b8dedd284323
Jeremy Jefferson <jeremyijefferson@gmail.com>
2024-04-26 08:44:22 -0700

    commit

--- TicTacToe/frontend/script.js
+++ TicTacToe/frontend/script.js
@@ -5,7 +5,10 @@ let currentPlayer = 'X';
 let board = ['', '', '', '', '', '', '', '', ''];
 let winner = null;
 
-const winningCombinations = [
+const player1 = 'X';
+const player2 = 'O';
+
+const winningCombos = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -13,18 +16,41 @@ const winningCombinations = [
   [1, 4, 7],
   [2, 5, 8],
   [0, 4, 8],
-  [2, 4, 6],
+  [2, 4, 6]
 ];
 
-const checkWinner = () => {
-  winningCombinations.forEach((combination) => {
-    const [a, b, c] = combination;
-    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-      winner = board[a];
-    }
+function handleClick() {
+  cells.forEach(cell => {
+    cell.addEventListener('click', (event) => {
+      if (cell.textContent === '') {
+        cell.textContent = currentPlayer;
+        const winner = determineWinner(winningCombos);
+        if (currentPlayer === player1) {
+          currentPlayer = player2;
+        } else {
+          currentPlayer = player1;
+        }
+      } else {
+        console.log(
+            "This spot is already occupied. Please choose another spot.");
+      }
+    });
   });
+}
 
-  if (winner) {
-    winnerDisplay.innerHTML = `Winner: ${winner}`;
+function determineWinner(winningCombos) {
+  for (i = 0; i < winningCombos.length; i++) {
+
+    console.log(winningCombos[i][0]);
+    console.log(winningCombos[i][1]);
+    console.log(winningCombos[i][2]);
   }
 }
+
+function playerMove() {
+      }
+
+// <div class="cell" id="cell-0">X</div>
+// [2, 4, 6]
+handleClick()
+determineWinner()
